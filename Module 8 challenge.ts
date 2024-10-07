47bca629ade39b068efd163e22ae6d832bd4ff67
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-10-07 14:12:50 -0500

    Add files via upload

---
+++ Module 8 challenge.ts
@@ -0,0 +1,218 @@
+// import the Vehicle, Motorbike, Car, Wheel, and AbleToTow classes/interfaces
+///import Vehicle from './Vehicle.js';
+
+
+
+/////import Car from './Car.js';
+
+//import AbleToTow from '../interfaces/AbleToTow.js';
+
+
+//import AbleToTow from './AbleToTow.js';
+
+
+  
+
+///import Motorbike from './Motorbike.js';
+
+
+
+
+
+
+
+
+//import Wheel from './Wheel.js';
+
+
+
+  
+
+
+
+
+
+
+
+
+// TODO: Declare properties of the Truck class
+// TODO: Declare properties of the Truck class
+
+
+
+
+constructor(make: string, model: string, year: number, payloadCapacity: number) {
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+  
+
+
+  // Constructor method to initialize properties
+  
+
+  // Constructor method to initialize properties
+  
+        
+  
+  
+  
+  
+
+  
+  
+
+
+
+ 
+
+
+  
+   
+  
+
+
+    
+
+
+  
+     
+ 
+  
+  
+      
+
+
+
+
+  
+
+
+
+  
+
+  // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
+  
+    
+  
+  
+  
+  
+
+
+
+
+  
+
+  ///constructor(
+  
+
+
+
+
+
+  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
+  
+  
+  
+  // TODO: Create a constructor that accepts the properties of the Truck class
+
+
+      
+      
+  
+    
+        
+
+  
+    
+      
+
+        
+
+        
+          
+    // TODO: The constructor should initialize the properties of the Truck class
+    
+    
+
+     // Constructor to initialize the Truck object
+     
+    
+
+   
+    
+      
+  
+      
+
+    // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
+
+
+// Export the Truck class as the default export
+
+
+
+
+// Car class that extends Vehicle class
+//class Car extends Vehicle {
+  // Declare properties of the Car class
+  //vin: string;
+  //color: string;
+  //make: string;
+  //model: string;
+  //year: number;
+  //weight: number;
+  //topSpeed: number;
+  //wheels: Wheel[];
+
+  // Constructor for the Car class
+  //constructor(
+    //vin: string,
+    //color: string,
+    //make: string,
+    //model: string,
+    //year: number,
+    //weight: number,
+    //topSpeed: number,
+    //wheels: Wheel[]
+  //) {
+    // Call the constructor of the parent class, Vehicle
+   // super();
+
+    // Initialize properties of the Car class
+    //this.vin = vin;
+    //this.color = color;
+    //this.make = make;
+    //this.model = model;
+    //this.year = year;
+    //this.weight = weight;
+    //this.topSpeed = topSpeed;
+    // Check if the wheels array has 4 elements
+    // If not, create 4 new Wheel objects
+    // Otherwise, use the provided wheels array
+    //if (wheels.length !== 4) {
+      //this.wheels = [new Wheel(), new Wheel(), new Wheel(), new Wheel()];
+   // } else {
+     // this.wheels = wheels;
+    
+  
+  
+
+}
+}
+
